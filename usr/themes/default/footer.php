009e64d1e1563d866480fe29809eb962a4f90d97
陈志 <2207860301@qq.com>
2019-01-09 10:12:27 +0800

    增加跳转工信部

--- usr/themes/default/footer.php
+++ usr/themes/default/footer.php
@@ -5,7 +5,7 @@
 </div><!-- end #body -->
 
 <footer id="footer" role="contentinfo">
-    &copy; <?php echo date('Y'); ?> ICP 备案/许可证编号为: 粤 ICP 备18135151号
+    &copy; <?php echo date('Y'); ?> ICP 备案/许可证编号为: <a href="http://www.miitbeian.gov.cn" target="_blank">粤 ICP 备18135151号</a>
     <?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动'); ?>.
 </footer><!-- end #footer -->
 
